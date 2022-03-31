c919821c347c8506ddda5b7ccbf5eb14b9328bb4
MissShin <jmshin67@uchicago.edu>
2022-03-31 16:12:44 -0500

    Create lass_1_testing.py

---
+++ lass_1_testing.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 31 10:29:45 2021
+
+@author: Jeff Levy
+"""
+
+print('Hello world!')
+
+print('No more math.')
